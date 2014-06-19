0bcb34ccd191a9e050c425647544d9c5f9283d78
Fredrik Thulin <fredrik@thulin.net>
2014-06-19 10:42:43 +0200

    bump version, and Jinja2 requirement

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = '0.2.5'
+version = '0.2.6-dev'
 
 requires = [
-    'Jinja2==2.7.2',
+    'Jinja2==2.7.3',
 ]
 
 testing_extras = [
